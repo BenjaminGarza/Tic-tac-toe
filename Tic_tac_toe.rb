4eca89f321582f0780980a480e89d032fd48b243
Miguel Prada <mapra99@gmail.com>
2019-08-23 17:33:48 -0500

    Added show method

--- Tic_tac_toe.rb
+++ Tic_tac_toe.rb
@@ -8,27 +8,46 @@ class Player
 
    end
 class Gameboard
-attr_accessor  :current_state
+attr_accessor  :current_state, :current_player
 
    def initialize
-    @@current_state = [["Empty","Empty","Empty"],["Empty","Empty","Empty"],["Empty","Empty","Empty"]]
+    @current_state = [["Empty","Empty","Empty"],["Empty","Empty","Empty"],["Empty","Empty","Empty"]]
 
+    @current_player = true
     end
 
 def turn(current_move, player_character)
   @current_move = current_move.split(",")
   @x = @current_move[0].to_i
   @y = @current_move[1].to_i
-  @@current_state[@x,@y] = player_character
-  
+  if @current_state[@x][@y] == "Empty"
+    @current_state[@x][@y] = player_character
+  else
+
+    puts "Please make a valid move"
+    break
+    @current_move = gets.chomp.split(",")
+    @x = @current_move[0].to_i
+    @y = @current_move[1].to_i
+  end
+  @current_player = !@current_player
+end
+
+def show
+(0..2).each do |i|
+  (0..2).each do |j|
+    print @current_state[i][j]
+    print "\t"
+  end
+  print "\n"
+end
 
 end
 
 
- 
 
- end   
 
+ end
 
 puts "Enter player one name"
     name_one = gets.chomp
@@ -42,14 +61,15 @@ player2 = Player.new("O",name_two)
 
 game = Gameboard.new
 
-puts game.current_state
-
+game.show
 
 puts "#{player1.name} VS #{player2.name}"
 
-puts "Make a move #{player1.name}"
-      current_move = gets.chomp
-      game.turn(current_move, player1.character)
+5.times do
+  current = game.current_player ? player1 : player2
+  puts "Make a move #{current.name}"
+        current_move = gets.chomp
+        game.turn(current_move, current.character)
 
-  
-      
+  game.show
+end
