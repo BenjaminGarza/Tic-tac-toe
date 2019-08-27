34e4e7d97b5c4cda665c14ea72636cec5a1880a6
Ben <benjisgarza@gmail.com>
2019-08-27 08:42:36 -0700

    Milestone 2, user interface

---
+++ -bin-/main.rb
@@ -0,0 +1,27 @@
+
+puts "Enter player one name"
+    name_one = gets.chomp
+
+puts "Enter player two name"
+    name_two = gets.chomp
+
+
+player1 = Player.new("X",name_one)
+player2 = Player.new("O",name_two)
+
+game = Gameboard.new
+
+game.show
+
+puts "#{player1.name} VS #{player2.name}"
+
+unless game.winner do
+  current = game.current_player ? player1 : player2
+  puts "Make a move #{current.name}"
+        current_move = gets.chomp
+        game.turn(current_move, current.character)
+
+  game.show
+end
+
+puts "#{winning_player} is the victor!"
